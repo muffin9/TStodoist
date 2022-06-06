29e14fdb369342a7114e0f5476dc36e53e980f86
muffin9 <jinlog9@gmail.com>
2022-06-06 18:57:06 +0800

    chore: import eslint 오름차순으로 자동 정렬되도록 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,17 @@ module.exports = {
     project: ['tsconfig.json'],
   },
   ignorePatterns: ['webpack.config.js', 'dist'],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'index'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'always',
+      },
+    ],
+  },
 };
