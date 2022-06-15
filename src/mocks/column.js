670a407638b8ba06ef1935ef791f8aac76a36978
Muffin <jinlog9@gmail.com>
2022-06-15 12:03:37 +0900

    feat: column ui 제작 & 더미데이터 노출

---
+++ src/mocks/column.js
@@ -0,0 +1,17 @@
+export const dummyColumns = [
+  {
+    id: 1,
+    title: 'todo',
+    isDeleted: false,
+  },
+  {
+    id: 2,
+    title: 'doing',
+    isDeleted: false,
+  },
+  {
+    id: 3,
+    title: 'done',
+    isDeleted: false,
+  },
+];
--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -1,1 +1,2 @@
 export const $ = (className: string) => document.querySelector(className);
+export const $$ = (id: string) => document.getElementById(id);
