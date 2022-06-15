85d2f4ed1a84abc647b7192e23db332907df8ae5
Muffin <jinlog9@gmail.com>
2022-06-15 12:07:11 +0900

    feat: column ui 제작 & 더미데이터 노출 (#16)

--- src/components/TodoColumn.ts
+++ src/components/TodoColumn.ts
@@ -1,7 +1,65 @@
+import { $ } from '@/utils/dom';
 export default class TodoColumn {
+  id: number;
+
+  title: string;
+
+  isdeleted: boolean;
+
+  date?: string;
+
+  count: number;
+
+  onModify: boolean;
+
+  constructor(id: number, title: string, isdeleted: boolean, date?: string) {
+    this.id = id;
+    this.title = title;
+    this.isdeleted = isdeleted;
+    this.date = date;
+    this.onModify = false;
+    this.count = 0;
+  }
+
+  setCount = (count: number) => {
+    this.count = count;
+  };
+
+  handleEventListener = () => {
+    $(`.${this.title} .column__title`)!.addEventListener(
+      'dblclick',
+      ({ target }: any) => {
+        const targetElement = target as HTMLDivElement | HTMLInputElement;
+        if (targetElement.classList.contains('column__title')) {
+          this.onModify = !this.onModify;
+          this.changeTitle();
+        }
+      },
+    );
+  };
+
+  changeTitle = () => {
+    const titleElement = $(`.${this.title} .column__title`);
+    if (this.onModify) {
+      titleElement!.outerHTML =
+        '<input class="column__title" type="input" maxlength="50" />';
+    }
+  };
+
   render = () => {
     return /* html */ `
-        <div class="column-wrapper">Column Area</div>
+    <div class="column-list ${this.title}">
+      <nav class="column">
+          <div class="column__left">
+              <div class="column__title">${this.title}</div>
+              <div class="column__count">${this.count}</div>
+          </div>
+          <div class="column__right">
+            <button type="button" class="column__add">+</button>
+            <button type="button" class="column__delete">x</button>
+          </div>
+      </nav>
+    </div>
     `;
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,66 @@
 import './style/index.scss';
 
+import TodoCard from './components/TodoCard';
+import { dummyColumns } from './mocks/column';
+import { dummyTodos } from './mocks/todos';
+
 import TodoColumn from '@/components/TodoColumn';
 import TodoHeader from '@/components/TodoHeader';
+import { $, $$ } from '@/utils/dom';
+
+interface IColumn {
+  id: number;
+  title: string;
+  isDeleted: boolean;
+}
+
+export interface ITodo {
+  id: number;
+  title: string;
+  content: string;
+  status: string;
+  userId?: number;
+  date: string;
+}
+
+type Todo = 'todo' | 'doing' | 'done';
+
+const root = $$('root')!;
+
+const createTodos = (column: any, todos: ITodo[]) => {
+  todos.forEach((todo: ITodo) => {
+    const newTodo = new TodoCard(
+      todo.id,
+      todo.title,
+      todo.content,
+      todo.status,
+      todo.date,
+    );
+    $(`.${column.title}`)!.insertAdjacentHTML('beforeend', newTodo.render());
+    newTodo.handleEventListener();
+  });
+};
+
+const createColumns = () => {
+  const columnWrapperElement = document.createElement('article');
+  columnWrapperElement.classList.add('column-wrapper');
+  root.appendChild(columnWrapperElement);
+
+  dummyColumns.forEach((data: IColumn) => {
+    const column = new TodoColumn(data.id, data.title, data.isDeleted);
+    const todoData = dummyTodos[data.title as Todo];
+    column.setCount(todoData.length);
+    columnWrapperElement.insertAdjacentHTML('beforeend', column.render());
+    column.handleEventListener();
+    createTodos(column, todoData);
+  });
+};
 
 const app = () => {
-  const root = document.getElementById('root')!;
   const header = new TodoHeader();
-  const column = new TodoColumn();
-  root.insertAdjacentHTML('afterend', column.render());
   root.insertAdjacentHTML('afterend', header.render());
   header.handleEventListener();
+  createColumns();
 };
 
 app();
