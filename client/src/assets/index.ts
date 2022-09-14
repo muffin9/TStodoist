1ce66548d924cb0fe26e6976cac0420a05d90595
Muffin <jinlog9@gmail.com>
2022-09-14 19:13:13 +0900

    feat: 🎨 로그인 컴포넌트 UI 제작 및 웹팩 설정 변경(이미지 불러오는 문제로 인해..)

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,4 +1,6 @@
 import Close from '@/assets/close.svg';
+import GoogleFocus from '@/assets/googleFocusImage.png';
+import GoogleNormal from '@/assets/googleNormalImage.png';
 import HoverClose from '@/assets/hover_close.svg';
 
-export { Close, HoverClose };
+export { Close, GoogleFocus, GoogleNormal, HoverClose };
