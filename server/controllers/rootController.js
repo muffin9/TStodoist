98347fc8878228fd55d82eeb3938b6a946c8ddf3
muffin9 <jinlog9@gmail.com>
2022-09-15 15:22:31 +0800

    feat: node.js + radis + passport 적용,직관적인 리다이렉트를 위해 프론트단 경로 단만 수정, index파일 src 밖으로 빼기

--- server/controllers/rootController.js
+++ server/controllers/rootController.js
@@ -1,9 +1,21 @@
+import fs from "fs";
+import path from "path";
 import connection from '../config/database.js';
+
+export const home = (req, res) => {
+    // req.session 이 없으면 로그인 화면으로 리다이렉트 시키기.
+    fs.readFile(path.join(path.resolve(), "../client/index.html"), (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).end(`<h1>ERROR</h1>`);
+        }
+        return res.status(200).end(data);
+    });
+}
 
 export const getData = (req, res) => {
     try {
         const userId = 1;
-
         const columnsQuery = `SELECT id, title FROM columns where user_id=${userId};`;
         const todosQuery = `SELECT * FROM todos where column_id in (select id from columns where user_id=${userId});`
         connection.query(columnsQuery + todosQuery, (err, datas, fileds) => {
@@ -11,10 +23,19 @@ export const getData = (req, res) => {
                 console.log(`query Error is ${err}...`);
                 return;
             }
-
             return res.json({columns: datas[0], todos: datas[1]});
         });
     } catch (err) {
         throw new Error(err);
     }
+}
+
+export const login = (req,res) => {
+    fs.readFile(path.join(path.resolve(), "../client/src/login.html"), (err, data) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).end(`<h1>ERROR</h1>`);
+        }
+        return res.status(200).end(data);
+    });
 }
