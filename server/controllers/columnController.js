b6b1ba1cc5b08268dfd2e34b02201d373ed834b4
muffin9 <jinlog9@gmail.com>
2022-09-16 16:53:13 +0800

    🚧 feat: 로그인시 디비 업데이트 기능 구현
    1.로그인 시 유저 테이블에 없는 유저라면 생성과 동시에 /로 리다이렉트
    2.로그인 페이지 접근시 로그인 세션이 존재한다면 /로 리다이렉트
    3. 루트 페이지 접근 시 로그인 세션이 존재하지 않다면 로그인 페이지로 리다이렉트

---
+++ server/controllers/columnController.js
@@ -0,0 +1,4 @@
+import connection from '../config/database.js';
+
+const createDefaultColumns = () => {
+}
--- server/routers/index.js
+++ server/routers/index.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getData, home, login } from "../controllers/rootController.js";
+import { getData, home, login, logout } from "../controllers/rootController.js";
 
 const rootRouter = express.Router();
 
-rootRouter.get('/', home)
+rootRouter.get('/', home);
 rootRouter.get("/datas", getData);
 rootRouter.get("/login", login);
+rootRouter.get("/logout", logout);
 
 export default rootRouter;
