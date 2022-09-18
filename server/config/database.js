b03e06aa72c32ed11c1dae8c7799383c7c0c11ed
Muffin <jinlog9@gmail.com>
2022-09-18 14:00:31 +0900

    build: DB port 추가.

--- server/config/database.js
+++ server/config/database.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const databaseConfig = {
     host     : process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
     user     : process.env.DATABASE_USER,
     password : process.env.DATABASE_PASSWORD,
     database : process.env.DATABASE_NAME,
